e7e61b7a83c910e1d01004508570667c2e8f2b56
FlyinSpaceWhale <millerzajdel31@gmail.com>
2025-07-29 00:57:36 +0000

    character counter function added to stats.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from stats import get_num_words
+from stats import get_book_text, get_num_words, get_num_char, text
 
 def main():
-    get_num_words()
+    char_count = get_num_char(text)
+    print(char_count)
     
-main()
+if __name__ == "__main__":
+    main()
--- stats.py
+++ stats.py
@@ -3,9 +3,22 @@ def get_book_text(filepath):
         file_content = f.read()
     return file_content
 
+text = get_book_text("./books/frankenstein.txt")
+
 def get_num_words():
-    letter_list = get_book_text("./books/frankenstein.txt").split()
+    letter_list = text.split()
     letter_count = len(letter_list)
 
     print(f"{letter_count} words found in the document")
 
+def get_num_char(string):
+    char_counts = {}
+    string = string.lower()
+    for char in string:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
+
+get_num_char(text)
